875dda0cb02e3d35da53bc5a5bee7ef203be3254
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-03-25 11:48:20 +0530

    created login page ui

---
+++ app/src/main/java/com/example/passvault/ui/screens/login/Login.kt
@@ -0,0 +1,91 @@
+package com.example.passvault.ui.screens.login
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.passvault.R
+
+@Composable
+fun Login(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(horizontal = 12.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = stringResource(R.string.app_name),
+            fontSize = 22.sp
+        )
+        Text(text = "Please enter login details to get started!")
+        Spacer(modifier = Modifier.height(20.dp))
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            label = { Text(text = "Email") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        OutlinedTextField(
+            value = "",
+            onValueChange = {},
+            label = { Text(text = "Password") },
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(12.dp))
+        Button(
+            onClick = {},
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(4.dp)
+        ) {
+            Text("Login")
+        }
+        Spacer(modifier = Modifier.height(22.dp))
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Text(text = "Don't have account? ")
+            Text(
+                text = "Create new account",
+                fontWeight = FontWeight.ExtraBold,
+                modifier = Modifier.clickable {
+                    //Todo : navigate to signup
+                }
+            )
+        }
+
+    }
+}
+
+@Composable
+@Preview
+fun LoginPreview() {
+    Surface {
+        Login()
+    }
+}
