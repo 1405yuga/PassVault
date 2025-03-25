265e1240f16a67a7b0fe8f8848f2e72deac3d7b5
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-03-25 12:45:05 +0530

    created bottom nav bar and displayed associated content

---
+++ app/src/main/java/com/example/passvault/ui/screens/add/Add.kt
@@ -0,0 +1,9 @@
+package com.example.passvault.ui.screens.add
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Add() {
+    Text("Add..")
+}
---
+++ app/src/main/java/com/example/passvault/ui/screens/home/Home.kt
@@ -0,0 +1,68 @@
+package com.example.passvault.ui.screens.home
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.outlined.List
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.passvault.ui.screens.add.Add
+import com.example.passvault.ui.screens.list.VaultList
+import com.example.passvault.ui.screens.profile.Profile
+
+
+@Composable
+fun Home(modifier: Modifier = Modifier) {
+    var selectedItem by rememberSaveable { mutableIntStateOf(0) }
+    val items = listOf("List", "Add", "Profile")
+    val iconsList = listOf(
+        Icons.AutoMirrored.Outlined.List,
+        Icons.Outlined.Add,
+        Icons.Filled.Person
+    )
+
+    Column(modifier = modifier.fillMaxSize()) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+        ) {
+            //todo: add animations on switch
+            when (selectedItem) {
+                0 -> VaultList()
+                1 -> Add()
+                2 -> Profile()
+            }
+        }
+        NavigationBar(modifier = Modifier.fillMaxWidth()) {
+            items.forEachIndexed { index, item ->
+                NavigationBarItem(
+                    icon = { Icon(iconsList[index], contentDescription = item) },
+                    selected = selectedItem == index,
+                    onClick = { selectedItem = index }
+                )
+            }
+        }
+    }
+}
+
+@Composable
+@Preview
+fun HomePreview() {
+    Surface {
+        Home()
+    }
+}
