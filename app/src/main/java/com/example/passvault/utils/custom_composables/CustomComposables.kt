182286a86210f0181d0c8b6ecb2d619fca8ecb4a
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-04-24 23:46:30 +0530

    added leading icon and solved the issue of extra spacing

--- app/src/main/java/com/example/passvault/utils/custom_composables/CustomComposables.kt
+++ app/src/main/java/com/example/passvault/utils/custom_composables/CustomComposables.kt
@@ -1,5 +1,6 @@
 package com.example.passvault.utils.custom_composables
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
@@ -12,8 +13,12 @@ import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.OffsetMapping
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TransformedText
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.sp
 
@@ -24,24 +29,29 @@ fun TextFieldWithErrorText(
     onTextChange: (String) -> Unit,
     errorMsg: String
 ) {
-    OutlinedTextField(
-        value = value,
-        onValueChange = onTextChange,
-        label = { Text(text = label) },
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
-        modifier = Modifier.fillMaxWidth(),
-        isError = errorMsg.isNotBlank(),
-        singleLine = true,
-        supportingText = {
-            if (errorMsg.isNotBlank()) {
-                Text(
-                    text = errorMsg,
-                    color = MaterialTheme.colorScheme.onErrorContainer,
-                    fontSize = 12.sp
-                )
-            }
-        }
+    Column(modifier = Modifier.fillMaxWidth()) {
+        OutlinedTextField(
+            value = value,
+            onValueChange = onTextChange,
+            label = { Text(text = label) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+            modifier = Modifier.fillMaxWidth(),
+            isError = errorMsg.isNotBlank(),
+            singleLine = true,
+        )
+        if (errorMsg.isNotBlank()) ErrorText(errorText = errorMsg)
+    }
+
+}
+
+@Composable
+fun ErrorText(errorText: String) {
+    Text(
+        text = errorText,
+        color = MaterialTheme.colorScheme.onErrorContainer,
+        fontSize = 12.sp
     )
+
 }
 
 @Composable
@@ -51,44 +61,79 @@ fun ShowAndHidePasswordTextField(
     onTextChange: (String) -> Unit,
     showPassword: Boolean,
     onShowPasswordClick: () -> Unit,
-    errorMsg: String
+    errorMsg: String,
+    leadingIcon: ImageVector? = null
 ) {
-    OutlinedTextField(
-        value = password,
-        onValueChange = onTextChange,
-        label = { Text(text = label) },
-        visualTransformation =
-            if (showPassword) VisualTransformation.None
-            else PasswordVisualTransformation(),
-        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-        modifier = Modifier.fillMaxWidth(),
-        singleLine = true,
-        trailingIcon = {
-            if (showPassword) {
-                IconButton(onClick = onShowPasswordClick) {
-                    Icon(
-                        imageVector = Icons.Filled.Visibility,
-                        contentDescription = "show password"
-                    )
-                }
-            } else {
-                IconButton(onClick = onShowPasswordClick) {
-                    Icon(
-                        imageVector = Icons.Filled.VisibilityOff,
-                        contentDescription = "hide password"
-                    )
-                }
-            }
-        },
-        isError = errorMsg.trim().isNotBlank(),
-        supportingText = {
-            if (errorMsg.isNotBlank()) {
-                Text(
-                    text = errorMsg,
-                    color = MaterialTheme.colorScheme.onErrorContainer,
-                    fontSize = 12.sp
-                )
-            }
+    Column(modifier = Modifier.fillMaxWidth()) {
+        if (leadingIcon == null) {
+            OutlinedTextField(
+                value = password,
+                onValueChange = onTextChange,
+                label = { Text(text = label) },
+                visualTransformation =
+                    if (showPassword) VisualTransformation.None
+                    else PasswordVisualTransformation(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                modifier = Modifier.fillMaxWidth(),
+                singleLine = true,
+                trailingIcon = {
+                    if (showPassword) {
+                        IconButton(onClick = onShowPasswordClick) {
+                            Icon(
+                                imageVector = Icons.Filled.Visibility,
+                                contentDescription = "show password"
+                            )
+                        }
+                    } else {
+                        IconButton(onClick = onShowPasswordClick) {
+                            Icon(
+                                imageVector = Icons.Filled.VisibilityOff,
+                                contentDescription = "hide password"
+                            )
+                        }
+                    }
+                },
+                isError = errorMsg.trim().isNotBlank(),
+            )
+        } else {
+            OutlinedTextField(
+                value = password,
+                onValueChange = onTextChange,
+                label = { Text(text = label) },
+                visualTransformation =
+                    if (showPassword) VisualTransformation.None
+                    else NoPaddingPasswordTransformation(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                modifier = Modifier.fillMaxWidth(),
+                singleLine = true,
+                trailingIcon = {
+                    if (showPassword) {
+                        IconButton(onClick = onShowPasswordClick) {
+                            Icon(
+                                imageVector = Icons.Filled.Visibility,
+                                contentDescription = "show password"
+                            )
+                        }
+                    } else {
+                        IconButton(onClick = onShowPasswordClick) {
+                            Icon(
+                                imageVector = Icons.Filled.VisibilityOff,
+                                contentDescription = "hide password"
+                            )
+                        }
+                    }
+                },
+                isError = errorMsg.trim().isNotBlank(),
+                leadingIcon = { Icon(imageVector = leadingIcon, contentDescription = label) }
+            )
         }
-    )
+        if (errorMsg.isNotBlank()) ErrorText(errorText = errorMsg)
+    }
+}
+
+class NoPaddingPasswordTransformation : VisualTransformation {
+    override fun filter(text: AnnotatedString): TransformedText {
+        val transformed = AnnotatedString("•".repeat(text.length))
+        return TransformedText(transformed, OffsetMapping.Identity)
+    }
 }
