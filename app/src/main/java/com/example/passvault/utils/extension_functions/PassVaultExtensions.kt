008f179155f858390eb09c11b3f8df463a922ee6
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-05-06 13:00:55 +0530

    created functions to imagevector to string and viceversa

---
+++ app/src/main/java/com/example/passvault/utils/extension_functions/PassVaultExtensions.kt
@@ -0,0 +1,30 @@
+package com.example.passvault.utils.extension_functions
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AttachMoney
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Language
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.PermMedia
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Work
+import androidx.compose.ui.graphics.vector.ImageVector
+
+fun ImageVector.toIconKey(): String {
+    return this.name
+}
+
+fun String.toOutlinedIcon(): ImageVector {
+    return when (this) {
+        Icons.Default.Home.name -> Icons.Default.Home
+        Icons.Default.Person.name -> Icons.Default.Person
+        Icons.Default.Language.name -> Icons.Default.Language
+        Icons.Default.Work.name -> Icons.Default.Work
+        Icons.Default.Lock.name -> Icons.Default.Lock
+        Icons.Default.AttachMoney.name -> Icons.Default.AttachMoney
+        Icons.Default.Favorite.name -> Icons.Default.Favorite
+        Icons.Default.PermMedia.name -> Icons.Default.PermMedia
+        else -> Icons.Default.Home
+    }
+}
