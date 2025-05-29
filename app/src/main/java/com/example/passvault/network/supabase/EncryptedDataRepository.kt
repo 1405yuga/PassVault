d5ebb0a8b62bed7573a83daa78abcbf8686d320b
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-05-29 13:08:44 +0530

    query implemented to get encryptedData via vaultId

--- app/src/main/java/com/example/passvault/network/supabase/EncryptedDataRepository.kt
+++ app/src/main/java/com/example/passvault/network/supabase/EncryptedDataRepository.kt
@@ -1,8 +1,10 @@
 package com.example.passvault.network.supabase
 
+import android.util.Log
 import com.example.passvault.data.EncryptedData
 import com.example.passvault.data.EncryptedDataTable
 import io.github.jan.supabase.SupabaseClient
+import io.github.jan.supabase.postgrest.from
 import io.github.jan.supabase.postgrest.postgrest
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -12,5 +14,21 @@ class EncryptedDataRepository @Inject constructor(private val supabaseClient: Su
 
     suspend fun insertEncryptedData(encryptedData: EncryptedData) {
         supabaseClient.postgrest[EncryptedDataTable.TABLE_NAME].insert(encryptedData)
+    }
+
+    suspend fun getAllEncryptedDataAtVaultId(vaultId: Long): List<EncryptedData>? {
+        Log.d(this.javaClass.simpleName, "Vault id recieved : $vaultId")
+        return try {
+            supabaseClient.from(EncryptedDataTable.TABLE_NAME)
+                .select {
+                    filter {
+                        eq("vault_id", vaultId)
+                    }
+                }
+                .decodeList<EncryptedData>()
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
     }
 }
