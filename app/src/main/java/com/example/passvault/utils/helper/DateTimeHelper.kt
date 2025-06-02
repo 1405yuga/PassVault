6f9abe8a86fcfb75fc7e0c9c34d31840dcbdbd86
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2025-06-02 18:05:25 +0530

    converted to asia/kolkata time zone

--- app/src/main/java/com/example/passvault/utils/helper/DateTimeHelper.kt
+++ app/src/main/java/com/example/passvault/utils/helper/DateTimeHelper.kt
@@ -3,6 +3,7 @@ package com.example.passvault.utils.helper
 import android.os.Build
 import androidx.annotation.RequiresApi
 import java.time.OffsetDateTime
+import java.time.ZoneId
 import java.time.format.DateTimeFormatter
 import java.util.Locale
 
@@ -16,16 +17,19 @@ object DateTimeHelper {
             else -> "${day}th"
         }
     }
+
     @RequiresApi(Build.VERSION_CODES.O)
     fun formatSupabaseTimestamp(input: String): String {
         val dateTime = OffsetDateTime.parse(input)
+        val istZone = ZoneId.of("Asia/Kolkata")
+        val istTime = dateTime.atZoneSameInstant(istZone)
 
-        val dayWithSuffix = getDayWithSuffix(dateTime.dayOfMonth)
-        val monthFormatter = DateTimeFormatter.ofPattern("MMM", Locale.ENGLISH) // Jan, Feb, etc.
-        val month = dateTime.format(monthFormatter)
-        val year = dateTime.year
+        val dayWithSuffix = getDayWithSuffix(istTime.dayOfMonth)
+        val monthFormatter = DateTimeFormatter.ofPattern("MMM", Locale.ENGLISH)
+        val month = istTime.format(monthFormatter)
+        val year = istTime.year
         val timeFormatter = DateTimeFormatter.ofPattern("h:mm a", Locale.ENGLISH)
-        val time = dateTime.format(timeFormatter)
+        val time = istTime.format(timeFormatter)
 
         return "$dayWithSuffix $month $year - $time"
     }
